6f6a315bb0264c72396bda3de72689a54c4aed7a
guozicheng@bilibili.com <guozicheng@bilibili.com>
2021-02-03 22:14:56 +0800

    config commit

---
+++ project_214/Assets/Script/ConfigMgr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigMgr
+{
+    private static ConfigMgr m_instance = null;
+    public static ConfigMgr GetInstance()
+    {
+        if(m_instance == null)
+        {
+            m_instance = new ConfigMgr();
+        }
+        return m_instance;
+    }
+
+    private Dictionary<int,QuestionConfig> m_questionDict;
+
+    private bool PasreQuestionConfig()
+    {
+        return true;
+    }
+}
